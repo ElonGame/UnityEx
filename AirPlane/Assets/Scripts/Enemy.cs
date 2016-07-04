5c304d1cb6b062044f6b0d5528c31a13842eabbc
great90 <897346026@qq.com>
2016-07-04 23:19:06 +0800

    增加游戏对象和攻击逻辑

---
+++ AirPlane/Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("MyGame/Enemy")]
+public class Enemy : MonoBehaviour {
+	// 移动速度
+	public float m_speed = 1;
+	// 旋转速度
+	public float m_rotSpeed = 30;
+	// 生命值
+	public float m_life = 10;
+
+	protected Transform m_transform;
+
+	// Use this for initialization
+	void Start () {
+		m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateMove();
+	}
+
+	protected void UpdateMove () {
+		// 左右移动
+		float rx = Mathf.Sin(Time.time) * Time.deltaTime;
+		// 前进
+		m_transform.Translate(new Vector3(rx, 0, -m_speed * Time.deltaTime));
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log("Enemy trigger");
+		if (other.tag.CompareTo("PlayerRocket") == 0) {
+			Rocket rocket = other.GetComponent<Rocket>();
+			if (rocket != null) {
+				m_life -= rocket.m_power;
+				if (m_life <= 0) {
+					Destroy(this.gameObject);
+				}
+			}
+			Debug.Log("Enemy trigger rocket");
+		}
+		else if (other.tag.CompareTo("Player") == 0) {
+			m_life = 0;
+			Destroy(this.gameObject);
+			Debug.Log("Enemy trigger player");
+		}
+	}
+}
--- AirPlane/Assets/Scripts/Player.cs
+++ AirPlane/Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	protected Transform m_transform;
 	public float m_speed = 1;
+	public Transform m_rocket;
+	public float m_rocketRate = 0.1f;
+	float m_rocketTime = 0;
+	public float m_life = 3;
 
 	// Use this for initialization
 	void Start () {
@@ -35,5 +39,21 @@ public class Player : MonoBehaviour {
 		}
 		// 移动
 		this.m_transform.Translate(new Vector3(moveh, 0, movev));
+		m_rocketTime -= Time.deltaTime;
+		if (m_rocketTime <= 0) {
+			m_rocketTime = m_rocketRate;
+			// 按空格键或鼠标左键发射子弹
+			if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) {
+				Instantiate(m_rocket, m_transform.position, m_transform.rotation);
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag.CompareTo("PlayerRocket") != 0) {
+			m_life -= 1;
+			if (m_life <= 0)
+				Destroy(this.gameObject);
+		}
 	}
 }
