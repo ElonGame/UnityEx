00168c3c44e2c725e3bc9b55fb6529a11e3a9ddf
great90 <897346026@qq.com>
2016-07-09 22:06:22 +0800

    补全漏提交文件

---
+++ AirPlane/Assets/Scripts/SuperEnemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("MyGame/SuperEnemy")]
+public class SuperEnemy : Enemy {
+
+	public Transform m_rocket;
+	public float m_fireSpeed = 2;
+	protected float m_fireTimer = 2;
+	protected Transform m_player;
+
+	void Awake () {
+		GameObject obj = GameObject.FindGameObjectWithTag("Player");
+		if (obj != null) {
+			m_player = obj.transform;
+		}
+		m_fireTimer = m_fireSpeed;
+	}
+
+	protected override void UpdateMove () {
+		m_fireTimer -= Time.deltaTime;
+		if (m_fireTimer <= 0) {
+			m_fireTimer = m_fireSpeed;
+			if (m_player != null) {
+				Vector3 relativePos = m_transform.position - m_player.position;
+				Instantiate(m_rocket, m_transform.position, Quaternion.LookRotation(relativePos));
+			}
+		}
+
+		// 前进
+		m_transform.Translate(new Vector3(0, 0, -m_speed * Time.deltaTime));
+	}
+}
